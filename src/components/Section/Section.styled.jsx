f0953008b9097f25c0e5909cc5dc7652e37b9da0
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-03-24 23:55:42 +0200

    Create Section.styled.jsx

---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const FeedbackSection = styled.section`
+  padding-top: 20px;
+  padding-bottom: 20px;
+`;
+export const Title = styled.h2`
+  margin-bottom: 20px;
+`;
